5d4c87b05e9dcff8b8e94e662cb706be167e373f
PavelLobanov-d <ploban@mail.ru>
2026-06-03 19:48:44 +0300

    Промежуточный коммит

---
+++ backend/DirectoryServiceBackend/src/DirectoryService.Infrastructure.PostgreSQL/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DirectoryService.Domain.Departments;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectoryService.Infrastructure.PostgreSQL.Configurations
+{
+    internal class DepartmentConfiguration: IEntityTypeConfiguration <Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+
+        }
+    }
+}
