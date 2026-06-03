47670f070f11c706cdf254a42875bf10f7acc93c
PavelLobanov-d <ploban@mail.ru>
2026-06-03 16:19:29 +0300

    Исправление замечаний

--- backend/DirectoryServiceBackend/src/DirectoryService.Infrastructure.PostgreSQL/Program.cs
+++ backend/DirectoryServiceBackend/src/DirectoryService.Infrastructure.PostgreSQL/Program.cs
@@ -1,39 +1,47 @@
-﻿using DirectoryService.Domain.Departments;
+﻿using DirectoryService.Domain.DepartmentLocations;
+using DirectoryService.Domain.DepartmentPositions;
+using DirectoryService.Domain.Departments;
 using DirectoryService.Domain.Locations;
 using DirectoryService.Domain.PositionsMatrix;
-using DirectoryService.Domain.DepartmentLocations;
-using DirectoryService.Domain.DepartmentPositions;
 using DirectoryService.Domain.shared;
 using DirectoryService.Infrastructure.PostgreSQL;
+using dotenv.net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
-var confbuilder = new ConfigurationBuilder();
-// установка пути к текущему каталогу
-confbuilder.SetBasePath(Directory.GetCurrentDirectory());
-// получаем конфигурацию из файла appsettings.json
-confbuilder.AddJsonFile("appsettings.json");
-IConfigurationRoot config = confbuilder.Build();
-// получаем строку подключения
-string? connectionString = config.GetConnectionString("PostgreSQL");
+
+DotEnv.Load();
+string? connectionString = Environment.GetEnvironmentVariable("DIRECTORY_SERVICE_CONNECTIONSTRING");
+Console.WriteLine($"connectionString: {connectionString}");
+
 if (connectionString == null)
 {
-    Console.WriteLine("Не указана строка подключения в поле \"PostgreSQL\"");
+    Console.WriteLine("Не указана строка подключения");
     return;
 }
 
-Console.WriteLine($"connectionString: {connectionString}");
 
-DirectoryServiceDbContext directoryServiceDbContext = new (connectionString);
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DirectoryServiceDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
-Location location = directoryServiceDbContext.Locations.Add(Location.Create(LocationName.Create("Локация1"), Address.Create("На деревню, Дедушке"))).Entity;
-PositionMatrix posCEO = directoryServiceDbContext.PositionsMatrix.Add(PositionMatrix.Create(PositionName.Create("Ген. директор"), Slug.Create("ceo"), parent: null)).Entity;
-Department departmentCEO = directoryServiceDbContext.Departments.Add(Department.Create(id: null, DepartmentName.Create("Аппарат ген.дира"), Slug.Create("director"), parent: null, posCEO)).Entity;
-DepartmentPosition depPos = directoryServiceDbContext.DepartmentPositions.Add(departmentCEO.ChiefDepartmentPosition).Entity;
-DepartmentLocation departmentLocation = directoryServiceDbContext.DepartmentLocations.Add(DepartmentLocation.Create(departmentCEO, location)).Entity;
+var app = builder.Build();
 
-PositionMatrix posDep11 = directoryServiceDbContext.PositionsMatrix.Add(PositionMatrix.Create(PositionName.Create("Начальник службы безопасности"), Slug.Create("chiefgbr"), posCEO)).Entity;
-Department department11 = directoryServiceDbContext.Departments.Add(Department.Create(id: null, DepartmentName.Create("Служба безопасности"), Slug.Create("gbr"), departmentCEO, posDep11)).Entity;
-depPos = directoryServiceDbContext.DepartmentPositions.Add(department11.ChiefDepartmentPosition).Entity;
-departmentLocation = directoryServiceDbContext.DepartmentLocations.Add(DepartmentLocation.Create(department11, location)).Entity;
+app.MapPost("/starttest", async (DirectoryServiceDbContext db) =>
+{
+    Location location = db.Locations.Add(Location.Create(LocationName.Create("Локация1"), Address.Create("На деревню, Дедушке"))).Entity;
+    PositionMatrix posCEO = db.PositionsMatrix.Add(PositionMatrix.Create(PositionName.Create("Ген. директор"), Slug.Create("ceo"), parent: null)).Entity;
+    Department departmentCEO = db.Departments.Add(Department.Create(id: null, DepartmentName.Create("Аппарат ген.дира"), Slug.Create("director"), parent: null, posCEO)).Entity;
+    DepartmentLocation departmentLocation = db.DepartmentLocations.Add(DepartmentLocation.Create(departmentCEO, location)).Entity;
+
+    PositionMatrix posDep11 = db.PositionsMatrix.Add(PositionMatrix.Create(PositionName.Create("Начальник службы безопасности"), Slug.Create("chiefgbr"), posCEO)).Entity;
+    Department department11 = db.Departments.Add(Department.Create(id: null, DepartmentName.Create("Служба безопасности"), Slug.Create("gbr"), departmentCEO, posDep11)).Entity;
+    departmentLocation = db.DepartmentLocations.Add(DepartmentLocation.Create(department11, location)).Entity;
+
+    await db.SaveChangesAsync().ConfigureAwait(true);
+});
+
+app.Run();
